71f10ceda6fc993e2334d60b25c7fd34bb6e7622
Ankus Choudhary <ankus.colab@gmail.com>
2026-06-22 13:01:14 +0530

    Create PROGRAM76.c

---
+++ week-3/DAY-19/PROGRAM76.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10];
+    int n, i, j;
+    int sum = 0;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of matrix:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + matrix[i][i];
+    }
+
+    printf("Sum of main diagonal elements = %d", sum);
+
+    return 0;
+}
